f94a581b0513a1b57ec5a9ba402428a52dce3e90
Ganeshdarshan Venkatraman Bhat <88202395+bhatganeshdarshan@users.noreply.github.com>
2023-11-07 07:41:55 +0530

    Time: 103 ms (80.53%), Space: 83.4 MB (76.27%) - LeetHub

---
+++ 2049-eliminate-maximum-number-of-monsters/2049-eliminate-maximum-number-of-monsters.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int eliminateMaximum(vector<int>& dist, vector<int>& speed) {
+        int n = dist.size();
+        vector<int> time(n);
+        for(int i =0 ; i < n ;i++){
+            time[i] = (dist[i]-1)/speed[i];
+        }
+        sort(time.begin(),time.end());
+        for(int i=0 ; i< n ;i++){
+            if(i > time[i]){
+                return i ;
+            }
+        }
+        return n ;
+    }
+};
