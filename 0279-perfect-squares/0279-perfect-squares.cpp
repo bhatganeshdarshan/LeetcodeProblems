a79e045dca18a17dd52402194e881aa46360efca
Ganeshdarshan Venkatraman Bhat <88202395+bhatganeshdarshan@users.noreply.github.com>
2023-11-25 19:01:10 +0530

    Time: 73 ms (85.69%), Space: 9.4 MB (79.42%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.cpp
+++ 0279-perfect-squares/0279-perfect-squares.cpp
@@ -1,18 +1,15 @@
 class Solution {
-    int F(int n){
+public:
+    int numSquares(int n) {
         vector<int> dp(n+1,0);
         dp[0]=1;
         for(int i = 1; i <= n ;i++){
             int res = INT_MAX;
-            for(int j = 1 ;j<=sqrt(n) ; j++){
+            for(int j = 1 ;j*j<=n ; j++){
                 res = (i-j*j>=0)?min(1+dp[i-j*j],res):res;
             }
             dp[i]=res;
         }
-        return dp[n] ;
-    }
-public:
-    int numSquares(int n) {
-        return F(n)-1;
+        return dp[n]-1 ;
     }
 };
